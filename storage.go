7ab24cfeeb8a11341b185ceffcec735eb11a93ce
Greivin López <greivin.lopez@gmail.com>
2014-09-28 19:51:46 -0600

    Using skue.Consume and skue.Produce methods

--- storage.go
+++ storage.go
@@ -75,7 +75,7 @@ func NewViewLayer(producer Producer, consumer Consumer) *ViewLayer {
 // Writes to the http writer according to what happens with the model
 // following the REST architectural style.
 func Create(view ViewLayer, model DatabasePersistor, w http.ResponseWriter, r *http.Request) {
-	err := view.Consumer.In(r, &model)
+	err := Consume(view.Consumer, w, r, &model)
 
 	if err != nil {
 		ServiceResponse(view.Producer, w, r, http.StatusBadRequest, fmt.Sprintf("Failed reading from request: %v", err))
@@ -103,7 +103,7 @@ func Read(view ViewLayer, model DatabasePersistor, cache MemoryCacher, w http.Re
 			ServiceResponse(view.Producer, w, r, http.StatusInternalServerError, fmt.Sprintf("Failed reading the item: %v", err))
 		}
 	} else {
-		view.Producer.Out(w, http.StatusOK, model)
+		Produce(view.Producer, w, r, http.StatusOK, model)
 	}
 }
 
@@ -113,7 +113,7 @@ func Read(view ViewLayer, model DatabasePersistor, cache MemoryCacher, w http.Re
 // Writes to the http writer according to what happens with the model
 // following the REST architectural style.
 func Update(view ViewLayer, model DatabasePersistor, cache MemoryCacher, w http.ResponseWriter, r *http.Request) {
-	err := view.Consumer.In(r, &model)
+	err := Consume(view.Consumer, w, r, &model)
 
 	if err != nil {
 		ServiceResponse(view.Producer, w, r, http.StatusBadRequest, fmt.Sprintf("Failed reading from request: %v", err))
@@ -162,6 +162,6 @@ func List(view ViewLayer, model DatabasePersistor, w http.ResponseWriter, r *htt
 	if err != nil {
 		ServiceResponse(view.Producer, w, r, http.StatusInternalServerError, fmt.Sprintf("Error requesting the list: %v", err))
 	} else {
-		view.Producer.Out(w, http.StatusOK, result)
+		Produce(view.Producer, w, r, http.StatusOK, result)
 	}
 }
